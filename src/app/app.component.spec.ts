4bc41dc50c6adf8fa5c5f85afc4475525e53b405
Jony <jonysamarelli@gmail.com>
2024-10-08 20:54:23 -0300

    PWA config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'PesquisaSatisfacao' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PesquisaSatisfacao');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
